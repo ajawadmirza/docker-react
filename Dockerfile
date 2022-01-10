023ba2b160a8e8e8ce39b2e12f337fbe9b137263
ajawadmirza <Abdul.Razzaque@visionetsystems.com>
2022-01-10 17:54:19 +0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine
+
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+
+# after build is done
+FROM nginx
+COPY --from=0 /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from jawad mirza test
         </p>
         <a
           className="App-link"
